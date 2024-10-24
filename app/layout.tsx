fb871fda66d8eb714f0c278b5f1851afabf5479a
Abderrahim GHAZALI <abderrahim.ghazali@dropteam.fr>
2024-10-25 00:25:03 +0200

    Update Metatags

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,15 +5,36 @@ import { cn } from "@/lib/utils";
 import { Navbar } from "@/components/navbar";
 import { Footer } from "@/components/footer";
 import { ThemeProvider } from "@/context/providers";
-import { Toaster } from "@pheralb/toast";
+
 import ToasterWrapper from "@/components/toaster-wrapper";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "MRRTracker",
-  description:
-    "MRRTracker is a simple macOS app that tracks your Monthly Recurring Revenue (MRR) from your toolbar, providing real-time updates, milestone notifications, and insights in a minimalist interface.",
-};
+  metadataBase: new URL('https://www.mrrtracker.app/'),
+  title:'MRRTracker',
+  description: 'MRRTracker is a simple macOS app that tracks your Monthly Recurring Revenue (MRR) from your toolbar, providing real-time updates, milestone notifications, and insights in a minimalist interface.',
+  keywords: ["MRRTracker", "MRR tracking", "Stripe", "macOS app", "business tracking", "macOS revenue tracker"],
+  openGraph: {
+    title: 'MRRTracker',
+    description: 'MRRTracker is a simple macOS app that tracks your Monthly Recurring Revenue (MRR) from your toolbar, providing real-time updates, milestone notifications, and insights in a minimalist interface.',
+    images: 'https://www.mrrtracker.app/preview.png',
+  },
+  twitter: {
+    card:'summary_large_image',
+    title: 'MRRTracker',
+    images: [{
+      url: 'https://www.mrrtracker.app/preview.png',
+      alt: 'A vibrant display of animated emojis showcasing various expressions and motions.',
+    }],
+    description: "MRRTracker is a simple macOS app that tracks your Monthly Recurring Revenue (MRR) from your toolbar, providing real-time updates, milestone notifications, and insights in a minimalist interface.",
+  },
+  icons: [
+    {
+      url: "/logo_2.png",
+      href: "/logo_2.png"
+    }
+  ]
+}
 
 export default function RootLayout({
   children,
