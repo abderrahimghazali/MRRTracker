dd5d20cb85eb057697874541edcba52c61458bef
Abderrahim GHAZALI <abderrahim.ghazali@dropteam.fr>
2024-10-25 04:00:52 +0200

    Add productHunt

---
+++ components/product-hunt-badge.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const ProductHuntBadge = () => {
+  return (
+    <a
+      href="https://www.producthunt.com/posts/mrrtracker?embed=true&utm_source=badge-featured&utm_medium=badge&utm_souce=badge-mrrtracker"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <img
+        src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=536622&theme=light"
+        alt="MRRTracker - Track MRR progress instantly on your macOS toolbar. | Product Hunt"
+        style={{ width: '250px', height: '54px' }}
+        width="250"
+        height="54"
+      />
+    </a>
+  );
+};
+
+export default ProductHuntBadge;
